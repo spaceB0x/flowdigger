c291f85dd9c7a3079cabc89095c13252a690cd10
spaceB0x <welton.tyler@gmail.com>
2015-11-16 11:37:12 -0600

    added dump function

--- nflow.h
+++ nflow.h
@@ -87,3 +87,26 @@ void pcap_fatal(const char *failed_in, const char *errbuf){
     exit(1);
 
 };
+
+/* Method to dump raw pcap data (Stolen from Jon Erikson) */
+void dump(const unsigned char *data_buffer, const unsigned int length) {
+   unsigned char byte;
+   unsigned int i, j;
+   for(i=0; i < length; i++) {
+      byte = data_buffer[i];
+      printf("%02x ", data_buffer[i]);  // Display byte in hex.
+      if(((i%16)==15) || (i==length-1)) {
+         for(j=0; j < 15-(i%16); j++)
+            printf("   ");
+         printf("| ");
+         for(j=(i-(i%16)); j <= i; j++) {  // Display printable bytes from line.
+            byte = data_buffer[j];
+            if((byte > 31) && (byte < 127)) // Outside printable char range
+               printf("%c", byte);
+            else
+               printf(".");
+         }
+         printf("\n"); // End of the dump line (each line is 16 bytes)
+      } // End if
+   } // End for
+};
